7ac8b3451f7d22dd1afa3d6e952bad72a4372e31
nik <lesnikowka@mail.ru>
2024-03-17 22:03:58 +0300

    opening closing filters

---
+++ CG_LESSON_1/CG_LESSON_1/ClosingFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG_LESSON_1
+{
+    class ClosingFilter : Filters
+    {
+        private float[,] kernel = null;
+        public ClosingFilter(float[,] kernel)
+        {
+            this.kernel = kernel;
+        }
+        public override Bitmap processImage(Bitmap sourceImage, BackgroundWorker worker)
+        {
+            var dFilter = new DilationFilter(kernel);
+            var eFilter = new ErosionFilter(kernel);
+
+            Bitmap resultImage = dFilter.processImage(sourceImage, worker, 50);
+            resultImage = eFilter.processImage(resultImage, worker, 50);
+
+            return resultImage;
+        }
+    }
+}
---
+++ CG_LESSON_1/CG_LESSON_1/DilationFilter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG_LESSON_1
+{
+    class DilationFilter : Filters
+    {
+        private float[,] kernel = null;
+        public DilationFilter(float[,] kernel)
+        {
+            this.kernel = kernel;
+        }
+        public override Bitmap processImage(Bitmap sourceImage, BackgroundWorker worker)
+        {
+            return processImage(sourceImage, worker, 100);
+        }
+
+        public Bitmap processImage(Bitmap sourceImage, BackgroundWorker worker, int percent)
+        {
+            Bitmap resultImage = new Bitmap(sourceImage.Width, sourceImage.Height);
+
+            for (int y = kernel.GetLength(1) / 2; y < sourceImage.Height - kernel.GetLength(1) / 2; y++)
+            {
+                worker.ReportProgress((int)((float)y / resultImage.Height * percent));
+
+                if (worker.CancellationPending)
+                {
+                    return null;
+                }
+
+                for (int x = kernel.GetLength(0) / 2; x < sourceImage.Width - kernel.GetLength(0) / 2; x++)
+                {
+                    int max = 0;
+                    for (int j = -kernel.GetLength(1) / 2; j < kernel.GetLength(1) / 2; j++)
+                    {
+                        for (int i = -kernel.GetLength(0) / 2; i < kernel.GetLength(0) / 2; i++)
+                        {
+                            if (kernel[i, j] > 0)
+                            {
+                                max = Math.Max(max, sourceImage.GetPixel(x + i, y + j).R);
+                            }
+                        }
+                    }
+
+                    max = Clamp(max, 0, 255);
+
+                    resultImage.SetPixel(x, y, Color.FromArgb(max, max, max));
+                }
+            }
+
+            return resultImage;
+        }
+    }
+}
